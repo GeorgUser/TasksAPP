afc5bc26ed0acba934088178687cd21e7f5c3b5e
George <sklavo.george@gmail.com>
2020-06-15 16:29:21 +0300

    add back-end for the app

---
+++ server/src/index.js
@@ -0,0 +1,59 @@
+import express from "express";
+import path from "path";
+import mongodb from "mongodb";
+import bodyParser from "body-parser";
+import dotenv from "dotenv";
+
+// https://server-georg.herokuapp.com/ | https://git.heroku.com/server-georg.git
+import tasks from "./routes/tasks";
+
+
+const app = express();
+
+dotenv.config({
+    path: path.join(__dirname, ".env")
+});
+
+const isDev = app.get("env") === "development";
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
+app.use("/api/tasks", tasks);
+
+
+// app.get("/", (req, res) => {
+//     res.setHeader('Access-Control-Allow-Origin', '*');
+//     console.log(res);
+//     console.log("Hi Heroku georg")
+// });
+
+app.get("/", (req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.sendFile(path.join(__dirname + '/data/index.html'));
+    console.log(process.env)
+});
+
+const port = process.env.PORT || 4000;
+const mongoUrl = `${process.env.DB_CONNECTION}`;
+
+
+mongodb.MongoClient.connect(mongoUrl, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+}).then(client => {
+        const db = client.db(process.env.DB_NAME);
+
+        app.set("db", db);
+
+        app.get("/", (req, res) => {
+            res.sendFile(path.join(__dirname, "./index.html"));
+        });
+        app.get("/api/test", (req, res) => {
+            res.json({ mes: "Hello from express" });
+        });
+        app.listen(port, () => console.log(`Running on localhost:${port}`));
+    })
+    .catch(err => console.log("Error connect"));
---
+++ server/src/routes/tasks.js
@@ -0,0 +1,103 @@
+import express from "express";
+import mongodb from "mongodb";
+import cors from "cors";
+
+const router = express.Router();
+
+const validate = data => {
+    const errors = {};
+    if (!data.title) errors.title = "Title filed can't be blank";
+    return errors;
+};
+
+router.options("*", cors());
+
+
+router.get("/", (req, res) => {
+    console.log("respons All Tasks");
+    const db = req.app.get("db");
+    db.collection("tasks")
+        .find({})
+        .toArray((err, tasks) => {
+            if (err) {
+                res.status(500).json({errors: {global: err}});
+                return;
+            }
+            res.setHeader('Access-Control-Allow-Origin', '*');
+            res.json({tasks});
+        });
+});
+
+// router.get("/:_id", (req, res) => {
+//     const db = req.app.get("db");
+//     db.collection("tasks").findOne(
+//         { _id: new mongodb.ObjectId(req.params._id) },
+//         (err, film) => {
+//             if (err) {
+//                 res.status(500).json({ errors: { global: err } });
+//                 return;
+//             }
+//             res.setHeader('Access-Control-Allow-Origin', '*');
+//             res.json({ film });
+//         }
+//     );
+// });
+
+router.post("/", (req, res) => {
+    console.log(req.body);
+    const db = req.app.get("db");
+    const errors = validate(req.body);
+
+    if (Object.keys(errors).length === 0) {
+        db.collection("tasks").insertOne(req.body, (err, r) => {
+            if (err) {
+                res.status(500).json({errors: {global: err}});
+                return;
+            }
+            console.log(r.ops[0]);
+            res.setHeader('Access-Control-Allow-Origin', '*');
+            res.json({task: r.ops[0]});
+        });
+    } else {
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        res.status(400).json({errors});
+    }
+});
+
+router.put("/:_id", (req, res) => {
+    const db = req.app.get("db");
+    const { _id, ...taskData } = req.body.task;
+
+    db.collection("tasks").findOneAndUpdate(
+        { _id: new mongodb.ObjectId(req.params._id) },
+        { $set: taskData },
+        { returnOriginal: false },
+        (err, r) => {
+            if (err) {
+                res.status(500).json({ errors: { global: err } });
+                return;
+            }
+            res.setHeader('Access-Control-Allow-Origin', '*');
+            res.json({ task: r.value });
+            console.log(r.value)
+        });
+});
+
+router.delete("/:_id", (req, res) => {
+    const db = req.app.get("db");
+    console.log(req.params);
+    console.log(req.params._id);
+    db.collection("tasks").deleteOne(
+        {_id: new mongodb.ObjectId(req.params._id)},
+        err => {
+            if (err) {
+                res.status(500).json({errors: {global: err}});
+                return;
+            }
+            res.setHeader('Access-Control-Allow-Origin', '*');
+            res.json({task: req.params});
+        }
+    );
+});
+
+export default router;
