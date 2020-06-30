5f5dabacd68afe779e523f94ef8c9db232019c45
George <sklavo.george@gmail.com>
2020-06-30 12:14:33 +0300

    logics transfer for TaskList

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,52 +1,27 @@
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 import NewTask from './forms/NewTask';
 import TasksList from './tasks/TasksList';
 import {Route} from "react-router-dom"
-import { connect } from "react-redux";
-import PropTypes from 'prop-types';
 import Nav from "./Nav";
 import LogIn from "./forms/LogIn";
-import {loadTasksAction} from "../actions/tasksAct";
+import SignUp from "./forms/SignUp";
 
 
-const App = ({tasks, loadTasksAction}) => {
+const App = () => {
 
-    const [taskForChange, getTaskForChange] = useState(null);
-
-    const done = tasks.filter(task => task.status === true);
-    const noDone = tasks.filter(task => task.status === false);
-
-    const changeTask = task => {
-        getTaskForChange(task);
-        console.log(taskForChange);
-    };
-
-    useEffect(() => {
-        loadTasksAction().catch(err => alert(err))
-    },[loadTasksAction]);
-
+    console.log('app === render');
     return (
     <div className="container">
         <div className="tableToDo">
             <Nav/>
             <Route exact path="/" component={LogIn} />
-            <Route path="/tasks" render={()=> <NewTask taskForChange={taskForChange}/>} />
-            <Route path="/tasks" render={()=><TasksList changeTask={changeTask} tasks={noDone} title={"Your tasks"}/>} />
-            <Route path="/history" render={()=><TasksList tasks={done} title={"History"}/>} />
+            <Route exact path="/SignUp" component={SignUp} />
+            <Route path="/tasks" render={(props)=> <NewTask {...props}/>} />
+            <Route path="/tasks" render={(props)=><TasksList {...props} state={false} title={"Your tasks"}/>} />
+            <Route path="/history" render={(props)=><TasksList {...props} state={true} title={"History"}/>} />
         </div>
     </div>)
 };
 
-function mapStateToProps(state) {
-    return{
-        tasks: state.tasks || [],
-    }
-}
 
-App.propTypes = {
-    tasks: PropTypes.array,
-    loadCoursesAction: PropTypes.func,
-};
-
-
-export default connect(mapStateToProps,{loadTasksAction})(App);
+export default App;
--- client/src/components/tasks/TasksList.js
+++ client/src/components/tasks/TasksList.js
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, {memo, useEffect} from 'react';
 import Task from './Task';
 import Message from './Message';
 import PropTypes from 'prop-types';
+import {connect} from "react-redux";
+import {loadTasksAction} from "../../actions/tasksAct";
 
-const TasksList = ({ tasks, title, changeTask }) => {
+const TasksList = memo(({ tasks, title, loadTasksAction }) => {
+
+    useEffect(() => {
+        if(tasks.length === 0) loadTasksAction().catch(err => alert(err));
+        // eslint-disable-next-line
+    },[]);
+
+    console.log('taskList === render');
     return (
         <>
             <h3>{title}</h3>
@@ -11,14 +20,20 @@ const TasksList = ({ tasks, title, changeTask }) => {
                 {tasks.length ? tasks.map((task, i) => (
                         <div key={task._id} className="task">
                             <span className="taskNum">{i+1}.</span>
-                            <Task task={task} changeTask={changeTask}/>
+                            <Task task={task}/>
                         </div>
                     )
                 ) : <Message/>}
             </ul>
         </>
     )
-};
+});
+
+function mapStateToProps(state, ownProps) {
+    return{
+        tasks: state.tasks.filter(task => task.status === ownProps.state) || [],
+    }
+}
 
 TasksList.propTypes = {
     tasks: PropTypes.array.isRequired,
@@ -29,4 +44,4 @@ TasksList.defaultProps = {
     title: "Tasks",
 };
 
-export default TasksList;
+export default connect(mapStateToProps, {loadTasksAction})(TasksList);
