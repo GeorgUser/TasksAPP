1c1110b869ed366e92b0ff279dd9850fcf5413d0
George <sklavo.george@gmail.com>
2020-06-30 11:50:31 +0300

    create form message

---
+++ client/src/components/forms/FormMessage.js
@@ -0,0 +1,20 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const FormMessage = ({type, children}) => (
+    <div className="message" style={{color: type === "error" ? "#e81216" : "#039a3c", fontSize: "10px"}}>
+        {children}
+    </div>
+);
+
+FormMessage.propTypes = {
+    type: PropTypes.string,
+    children: PropTypes.string
+};
+
+FormMessage.defaultProps = {
+    type: "error",
+    children: " ",
+};
+
+export default FormMessage;
