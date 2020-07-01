4906c40e15d7fcced544fd44305efdad10d0650f
George <sklavo.george@gmail.com>
2020-07-01 16:14:56 +0300

    change path to the server

--- client/src/api.js
+++ client/src/api.js
@@ -3,9 +3,9 @@ import axios from "axios";
 
 const api = {
     tasks: {
-        allTasks: () => axios.get("http://localhost:4000/api/tasks").then(res => res.data.tasks),
+        allTasks: () => axios.get("http://18.218.143.75:4000/api/tasks").then(res => res.data.tasks),
         create: task => axios({
-            url: "http://localhost:4000/api/tasks",
+            url: "http://18.218.143.75:4000/api/tasks",
             method: 'POST', // *GET, POST, PUT, DELETE, etc.
             mode: 'cors', // no-cors, cors, *same-origin
             cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
@@ -17,12 +17,12 @@ const api = {
             referrer: 'no-referrer', // no-referrer, *client
             data: JSON.stringify(task)
         }).then(res => res.data.task),
-        update: task => axios.put(`http://localhost:4000/api/tasks/${task._id}`, {task}).then(res => res.data.task),
-        delete: task => axios.delete(`http://localhost:4000/api/tasks/${task._id}`).then(res => res.data.task),
+        update: task => axios.put(`http://18.218.143.75:4000/api/tasks/${task._id}`, {task}).then(res => res.data.task),
+        delete: task => axios.delete(`http://18.218.143.75:4000/api/tasks/${task._id}`).then(res => res.data.task),
     },
     users: {
         create: user => axios({
-            url: "http://localhost:4000/api/users",
+            url: "http://18.218.143.75:4000/api/users",
             method: 'POST', // *GET, POST, PUT, DELETE, etc.
             mode: 'cors', // no-cors, cors, *same-origin
             cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
@@ -35,7 +35,7 @@ const api = {
             data: JSON.stringify({user: {...user}})
         }),
         login: credentials => axios({
-            url: "http://localhost:4000/api/auth",
+            url: "http://18.218.143.75:4000/api/auth",
             method: 'POST', // *GET, POST, PUT, DELETE, etc.
             mode: 'cors', // no-cors, cors, *same-origin
             cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
